f692531b2a6be2044d634096731ec348b8181950
Andrey <Koshelev-1969@mail.ru>
2023-03-03 20:36:09 +0400

    OOP

--- culinary/webs/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.blogweb, name='blogweb'),
-    path('blog/<str:pk>/', views.blog, name='blog'),
-
-    path('current/', views.currentweb, name='currentweb'),
-]
